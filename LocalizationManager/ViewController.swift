c700ff35dfd0a8b8f22dc5672db7cc46f9718a23
Mohamed Samir <msalem@egypt.itgsolutions.com>
2020-09-13 10:26:15 +0200

    Initial Commit

---
+++ LocalizationManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalizationManager
+//
+//  Created by Mohamed Samir on 9/13/20.
+//  Copyright © 2020 Mohamed Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
